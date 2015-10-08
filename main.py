102dc49729012d81324d6093a77a9e4f3d34dc70
shinshinshin <saekishinya@yahoo.co.jp>
2015-10-09 04:52:57 +0900

    on the way

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 #-*- coding: utf-8 -*-
 import math
 
+class Pos:
+	def __init__(self, x = 0.0, y = 0.0, z = 0.0):
+		self.x = round(x,3)
+		self.y = round(y,3)
+		self.z = round(z,3)
+
 #calculate hand position acording to arm rotation
 
 # calc right arm
@@ -9,7 +15,8 @@ import math
 # x beside horizontal leaving +
 # y forward horizontal forward + 
 # z vertical upper +
-def elbow_pos(l1,w1,t1):
+def elbow_pos(w1,t1):
+	l1 = 20
 	#1 only wing w1
 	# 0 - direct to ground
 	# 90 - direct to horizontal
@@ -19,12 +26,17 @@ def elbow_pos(l1,w1,t1):
 	# 90 - direct to forward
 
 	x = l1 * math.sin(w1)
-	y = l1 * math.sin(t1)
-	z = l1 * (math.cos(w1) - 1) * ( -1 * math.cos(t1))
+	y = l1 * math.sin(t1) * math.cos(w1)
+	z = l1 * (math.cos(w1)) * ( -1 * math.cos(t1))
 
-	return [x,y,z]
+	return Pos(x,y,z)
 
-def hand_pos(l1,w1,t1,l2,w2,t2)
+def hand_pos(w1,t1,w2,t2):
+	l2 = 20
+	w1 = into_radian(w1)
+	t1 = into_radian(t1)
+	w2 = into_radian(w2)
+	t2 = into_radian(t2)
 	#1 calc w1 0 t1 0
 	# w2
 	# 0 - direct to ground
@@ -32,9 +44,6 @@ def hand_pos(l1,w1,t1,l2,w2,t2)
 	# t2
 	# 0 - direct to myself
 	# 90 - direct to forward
-	z = l2 * (math.cos(w2) - 1)
-	y = l2 * math.sin(t2) * math.sin(w2)
-	x = l2 * math.sin(w2) * (-1 * math.cos(t2))
 
 	#1 calc w1  t1 
 	# w2
@@ -43,19 +52,16 @@ def hand_pos(l1,w1,t1,l2,w2,t2)
 	# t2
 	# 0 - direct to myself
 	# 90 - direct to forward
-	l1_l = elbow_pos(w1,t1)
-	l1_x = l1_l[0]
-	l1_y = l1_l[1]
-	l1_z = l1_l[2]
+	l1_p = elbow_pos(w1,t1)
 	
-	z = l1_z + l2 * (math.cos(w1-w2) -1) * ( -1 * math.cos(t1))
-	y = l1_y + l2 * math.sin(w2-w1) * math.sin(t2) * math.sin(t1)
-	x = l1_x + l2 * math.sin(w1-w2) * ( -1 * math.cos(t2))
+	x = l1_p.x + l2 * math.sin(w1-w2) * math.cos(t2)
+	y = l1_p.y + l2 * math.cos(w1-w2) * math.sin(t2)
+	z = l1_p.z + l2 * (math.cos(w1-w2)) * ( -1 * math.cos(t2))
 
-	return [x,y,z]
+	return Pos(x,y,z)
 
 def into_radian(x):
-	return math.pi * x / 180.0 
+	return math.pi * float(x) / 180.0 
 
 #print(hand_pos(1,0,0,0,0,0))
 #print(hand_pos(2,0,0,0,0,0))
--- test.py
+++ test.py
@@ -5,14 +5,130 @@ import unittest
 
 import main
 
+# x beside horizontal leaving +
+# y forward horizontal forward + 
+# z vertical upper +
+r_2 = 1.41421356
+r_3 = 1.7320508
 class Test_Hand_Pos(unittest.TestCase):
 	def setup(self):
-		self.hand_pos = main.hand_pos
+		pass
 
-	def test_hand_pos(self):
-		expected = 1 
-		actual = self.hand_pos(1,0,0,0,0,0)
-		self.assertEqual(expected, actual)
+	def test_elbow_pos_1(self):
+		expected = main.Pos(0,0,-20)
+		actual = main.elbow_pos(0,0)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_elbow_pos_2(self):
+		expected = main.Pos(0,20,0)
+		w1 = main.into_radian(0)
+		t1 = main.into_radian(90)
+		actual = main.elbow_pos(w1,t1)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_elbow_pos_3(self):
+		expected = main.Pos(20,0,0)
+		w1 = main.into_radian(90)
+		t1 = main.into_radian(0)
+		actual = main.elbow_pos(w1,t1)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_elbow_pos_4(self):
+		expected = main.Pos(r_2/2.0*20,0,-r_2/2.0*20)
+		w1 = main.into_radian(45)
+		t1 = main.into_radian(0)
+		actual = main.elbow_pos(w1,t1)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_elbow_pos_5(self):
+		expected = main.Pos(0,r_2/2.0*20,-1 * r_2/2.0*20)
+		w1 = main.into_radian(0)
+		t1 = main.into_radian(45)
+		actual = main.elbow_pos(w1,t1)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_elbow_pos_6(self):
+		expected = main.Pos(1.0/2.0*20,0,-1 *r_3/2.0*20)
+		w1 = main.into_radian(30)
+		t1 = main.into_radian(0)
+		actual = main.elbow_pos(w1,t1)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_elbow_pos_7(self):
+		expected = main.Pos(1.0/2.0*20,r_3/2.0*r_3/2.0*20,-1 * r_3/2.0*20/2.0)
+		w1 = main.into_radian(30)
+		t1 = main.into_radian(60)
+		actual = main.elbow_pos(w1,t1)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+# x beside horizontal leaving +
+# y forward horizontal forward + 
+# z vertical upper +
+	def test_hand_pos_1(self):
+		expected = main.Pos(0,0,-40)
+		actual = main.hand_pos(0,0,0,0)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_hand_pos_2(self):
+		expected = main.Pos(0,20,20)
+		actual = main.hand_pos(0,0,90,90)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	"""
+	def test_hand_pos_3(self):
+		expected = main.Pos(40,0,0)
+		actual = main.hand_pos(90,0,0,0)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_hand_pos_4(self):
+		expected = main.Pos(r_2/2.0*40.0,0,-1 * r_2/2.0*40)
+		actual = main.hand_pos(45,0,0,0)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_hand_pos_5(self):
+		expected = main.Pos(0,r_2/2.0*40,-1 *r_2/2.0*40)
+		actual = main.hand_pos(0,45,0,0)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_hand_pos_6(self):
+		expected = main.Pos(1.0/2.0*40,0,-r_3/2.0*40)
+		actual = main.hand_pos(0,30,0,0)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+
+	def test_hand_pos_7(self):
+		expected = main.Pos(1.0/2.0*40,r_3/2.0*r_3/2.0*40,-r_3/2.0*40/2.0)
+		actual = main.hand_pos(30,60,0,0)
+		self.assertEqual(expected.x, actual.x)
+		self.assertEqual(expected.y, actual.y)
+		self.assertEqual(expected.z, actual.z)
+	"""
+
 
 if __name__ == "__main__":
 	    unittest.main()
